8f8b2780178c6dd1ed757d25ef7aad1ff3c0b936
Lucas Timotio <lucastimotio@gmail.com>
2016-08-11 10:32:36 -0300

    #10

--- src/Controller/BirthdaysOfTheMonthController.php
+++ src/Controller/BirthdaysOfTheMonthController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Controller;
 
 use Cake\Event\Event;
@@ -6,23 +7,18 @@ use App\Controller\AppController;
 use Cake\Controller\Component\FlashComponent;
 use Cake\Datasource\ConnectionManager;
 
-class BirthdaysOfTheMonthController extends AppController
+class BirthdaysOfTheMonthController extends AppController 
 {
-    public function viewpdf()
+    public function viewpdf() 
     {
-
-    	$birthdaysOfTheMonthForm = null;
-    	foreach ($this->request->data as $key )
-    	{
-    		$birthdaysOfTheMonthForm = $key;	    		
-    	}
-        
-        $birthdaysOfTheMonthForm = intval($birthdaysOfTheMonthForm) +1;
-    	$birthdaysOfTheMonthForm = (string) $birthdaysOfTheMonthForm;
-
-		$connection = ConnectionManager::get('baseProtheus');
-
-		$birthdaysOfTheMonth = null;
+        $birthdaysOfTheMonthForm = null;
+        foreach ($this->request->data as $key) {
+            $birthdaysOfTheMonthForm = $key;
+        }
+        $birthdaysOfTheMonthForm = intval($birthdaysOfTheMonthForm) + 1;
+        $birthdaysOfTheMonthForm = (string) $birthdaysOfTheMonthForm;
+        $connection = ConnectionManager::get('baseProtheus');
+        $birthdaysOfTheMonth = null;
         $birthdaysOfTheMonth = $connection->execute("SELECT [RA_NOME]
 			,CONVERT(varchar(10),(DAY([RA_NASC])))+'/'+CONVERT(varchar(10),(MONTH([RA_NASC])))+'/'+CONVERT(varchar(10),YEAR([RA_NASC])) 
 			    AS DataDeNascimento
@@ -32,14 +28,12 @@ class BirthdaysOfTheMonthController extends AppController
 			WHERE MONTH([RA_NASC]) = '$birthdaysOfTheMonthForm'
 			AND [RA_SITFOLH] != 'D'
 			ORDER BY DAY([RA_NASC]), [RA_NOME]");
-    
-        $this->set(compact('birthdaysOfTheMonth','birthdaysOfTheMonthForm'));
-        $this->set('_serialize', ['birthdaysOfTheMonth','birthdaysOfTheMonthForm']);
-
+        $this->set(compact('birthdaysOfTheMonth', 'birthdaysOfTheMonthForm'));
+        $this->set('_serialize', ['birthdaysOfTheMonth', 'birthdaysOfTheMonthForm']);
         $this->viewBuilder()->layout('ajax');
-        $this->response->type('pdf');	
-	}
-
+        $this->response->type('pdf');
+    }
 
 }
+
 ?>
--- src/Controller/NoticesController.php
+++ src/Controller/NoticesController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Controller;
 
 use Cake\Event\Event;
@@ -11,29 +12,29 @@ use Cake\Datasource\ConnectionManager;
  *
  * @property \App\Model\Table\NoticesTable $Notices
  */
-class NoticesController extends AppController
+class NoticesController extends AppController 
 {
     public $paginate = [
         'limit' => 5];
+
     /**
      * Index method
      *
      * @return \Cake\Network\Response|null
      */
-    public function index()
-    {        
+    public function index() {
         $authenticatedUserId = $this->Auth->user('id');
         $notices = $this->paginate($this->Notices->find()
-            ->where(['user_id'=> $authenticatedUserId])
-            ->order(['id'=>'DESC'])
+                        ->where(['user_id' => $authenticatedUserId])
+                        ->order(['id' => 'DESC'])
         );
         $noticesUsers = $this->Notices->find()
-            ->limit(4)
-            ->select(['notices.id','notices.subject','notices.text','notices.created','users.name'])
-            ->innerJoin('notices_users', 'notices.id = notices_users.notice_id')
-            ->innerJoin('users', 'users.id = notices.user_id')
-            ->where(['notices_users.user_id'=> $authenticatedUserId])
-            ->order(['notices.id'=>'DESC']);
+                ->limit(4)
+                ->select(['notices.id', 'notices.subject', 'notices.text', 'notices.created', 'users.name'])
+                ->innerJoin('notices_users', 'notices.id = notices_users.notice_id')
+                ->innerJoin('users', 'users.id = notices.user_id')
+                ->where(['notices_users.user_id' => $authenticatedUserId])
+                ->order(['notices.id' => 'DESC']);
         $connection = ConnectionManager::get('default');
         $noticesRoles = $connection->execute("
         SELECT DISTINCT TOP 4
@@ -46,10 +47,10 @@ class NoticesController extends AppController
         FROM [integratedSystemQualitex].[dbo].[notices]
         INNER JOIN [integratedSystemQualitex].[dbo].[notices_roles] ON [notices].[id] = [notices_roles].[notice_id]
         INNER JOIN [integratedSystemQualitex].[dbo].[users] ON [users].[id] = [notices].[user_id]
-        WHERE [notices_roles].[role_id] IN (SELECT [role_id] FROM [integratedSystemQualitex].[dbo].[roles_users] WHERE [user_id] = ".$authenticatedUserId.")
+        WHERE [notices_roles].[role_id] IN (SELECT [role_id] FROM [integratedSystemQualitex].[dbo].[roles_users] WHERE [user_id] = " . $authenticatedUserId . ")
           ORDER BY [id] DESC");
-        $this->set(compact('notices','noticesUsers','noticesRoles'));
-        $this->set('_serialize', ['notices','noticesUsers','noticesRoles']);
+        $this->set(compact('notices', 'noticesUsers', 'noticesRoles'));
+        $this->set('_serialize', ['notices', 'noticesUsers', 'noticesRoles']);
     }
 
     /**
@@ -64,13 +65,13 @@ class NoticesController extends AppController
         $authenticatedUserId = $this->Auth->user('id');
         $notice = $this->Notices->get($id, [
             'contain' => ['Users', 'Roles']
-        ]);        
+        ]);
         $creatorName = $this->Notices->find()
             ->select('users.name')
             ->innerJoin('users', 'users.id = notices.user_id')
-            ->where(['notices.id'=> $id]);
-        $this->set(compact('notice','authenticatedUserId','creatorName'));
-        $this->set('_serialize', ['notice', 'authenticatedUserId','creatorName']);
+            ->where(['notices.id' => $id]);
+        $this->set(compact('notice', 'authenticatedUserId', 'creatorName'));
+        $this->set('_serialize', ['notice', 'authenticatedUserId', 'creatorName']);
     }
 
     /**
@@ -81,22 +82,24 @@ class NoticesController extends AppController
     public function add()
     {
         $authenticatedUser = $this->Auth->user();
-        debug($authenticatedUser);
         $notice = $this->Notices->newEntity();
-        if ($this->request->is('post')) {
+        if ($this->request->is('post'))
+        {
             $notice = $this->Notices->patchEntity($notice, $this->request->data);
-            if ($this->Notices->save($notice)) {
+            if ($this->Notices->save($notice)) 
+            {
                 $this->Flash->success(__('A notícia foi salva!'));
-
                 return $this->redirect(['action' => 'index']);
-            } else {
+            } 
+            else 
+            {
                 $this->Flash->error(__('A notícia não pôde ser salva. Por Favor, tente novamente.'));
             }
         }
         $users = $this->Notices->Users->find('list', ['limit' => 200]);
         $roles = $this->Notices->Roles->find('list', ['limit' => 200]);
-        $this->set(compact('notice', 'users', 'roles','authenticatedUser'));
-        $this->set('_serialize', ['notice','authenticatedUser']);
+        $this->set(compact('notice', 'users', 'roles', 'authenticatedUser'));
+        $this->set('_serialize', ['notice', 'authenticatedUser']);
     }
 
     /**
@@ -106,17 +109,21 @@ class NoticesController extends AppController
      * @return \Cake\Network\Response|void Redirects on successful edit, renders view otherwise.
      * @throws \Cake\Network\Exception\NotFoundException When record not found.
      */
-    public function edit($id = null)
+    public function edit($id = null) 
     {
         $notice = $this->Notices->get($id, [
             'contain' => ['Users', 'Roles']
         ]);
-        if ($this->request->is(['patch', 'post', 'put'])) {
+        if ($this->request->is(['patch', 'post', 'put'])) 
+        {
             $notice = $this->Notices->patchEntity($notice, $this->request->data);
-            if ($this->Notices->save($notice)) {
+            if ($this->Notices->save($notice)) 
+            {
                 $this->Flash->success(__('A notícia foi salva!'));
                 return $this->redirect(['action' => 'index']);
-            } else {
+            } 
+            else 
+            {
                 $this->Flash->error(__('A notícia não pôde ser salva. Por Favor, tente novamente.'));
             }
         }
@@ -133,30 +140,33 @@ class NoticesController extends AppController
      * @return \Cake\Network\Response|null Redirects to index.
      * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
      */
-    public function delete($id = null)
+    public function delete($id = null) 
     {
         $this->request->allowMethod(['post', 'delete']);
         $notice = $this->Notices->get($id);
-        if ($this->Notices->delete($notice)) {
+        if ($this->Notices->delete($notice)) 
+        {
             $this->Flash->success(__('A notícia foi apagada!'));
-        } else {
+        }
+        else 
+        {
             $this->Flash->error(__('A notícia não pôde ser apagada. Por Favor, tente novamente.'));
         }
         return $this->redirect(['action' => 'index']);
     }
 
-    public function beforeFilter(Event $event)
+    public function beforeFilter(Event $event) 
     {
         parent::beforeFilter($event);
         // Allow users to register and logout.
         // You should not add the "login" action to allow list. Doing so would
         // cause problems with normal functioning of AuthComponent.
-
         //$this->Auth->allow(['logout','login']);  
     }
 
-    public function isAuthorized($user)
+    public function isAuthorized($user) 
     {
-      return parent::isAuthorized($user);
+        return parent::isAuthorized($user);
     }
+
 }
